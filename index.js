0be1befcffeaa1adfbc2336c8a4bf9c4dd4cb568
shaber ahmad <shaberahmad2019s@gmail.com>
2023-08-19 17:45:37 +0600

    some basic array

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const myarray = ['java','javaScript','python','c++','go']
-const newArray = [1,2,3,4,5,6,,7,]
+const newArray = [1,2,3,4,5,6,7,]
 
 console.log(newArray.includes(10))
 
@@ -8,3 +8,5 @@ console.log(newArray.indexOf(4))
 console.log(typeof newArray.join())
 
 console.log(myarray.length)
+
+console.log(newArray.push(9))
