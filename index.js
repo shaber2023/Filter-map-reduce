df9224daaef2c7f3b42ed746bf59a0a4b9c7ea1a
shaber ahmad <shaberahmad2019s@gmail.com>
2023-08-19 21:16:20 +0600

    intermediet function

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const myarray = ['java','javaScript','python','c++','go']
-const newArray = [1,2,3,4,5,6,7,]
 
-//same output sprade oparetor
-const data = [...myarray,...newArray];
-console.log(data)
-
-//same output concat oparetor
-const mydata = myarray.concat(newArray);
-console.log(mydata)
-
+function mydata (anyobj){
+    return `my name is ${anyobj.name} my home is ${anyobj.home}`
+}
+console.log(mydata({name:'shaber',home:'khulna'}))
