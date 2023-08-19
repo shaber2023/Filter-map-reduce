e9a82b7535357799f90d05310aafc2ffc490c96b
shaber ahmad <shaberahmad2019s@gmail.com>
2023-08-19 13:44:50 +0600

    some modify

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const myarray = ['java','javaScript','python','c++','go']
+const newArray = [1,2,3,4,5,6,,7,]
 
-//print array data
-myarray.map((item)=>console.log(item))
-myarray.filter((item)=>console.log(item))
-myarray.forEach((item)=>console.log(item))
-for (const iterator of myarray) {
-    console.log(iterator)
-}
+console.log(newArray.includes(10))
 
-// forEach callbackfnction value: string, index: number, array: string
-myarray.forEach(function(item,index,array){
-    console.log(item)
-})
+console.log(newArray.indexOf(4))
 
-myarray.filter((item)=>{
-    console.log(item)
-})
+console.log(typeof newArray.join())
 
-const myNumber = [2,4,6,9,54,32,95,22,91]
-
-const newValue = myNumber.map((item)=>item+10);
-console.log(newValue)
-
-const Number = [2,4,6,9,54,32,95,22,91]
-
-//chaining
-const Value = Number.map((item)=>item+10).filter((item)=>item>50);
-console.log(Value)
+console.log(myarray.length)
