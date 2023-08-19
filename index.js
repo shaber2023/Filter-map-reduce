8b0c1306b8a672aca72e96c0f5fdf6ee12c0d383
shaber ahmad <shaberahmad2019s@gmail.com>
2023-08-19 21:23:06 +0600

    intermediet function with array

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 
-function mydata (anyobj){
-    return `my name is ${anyobj.name} my home is ${anyobj.home}`
+// function mydata (anyobj){
+//     return `my name is ${anyobj.name} my home is ${anyobj.home}`
+// }
+// console.log(mydata({name:'shaber',home:'khulna'}))
+
+const arrayData = [12,34,56,43,22];
+
+function myarray(anyary){
+    return console.log(`print my array data ${anyary[2]}`)
 }
-console.log(mydata({name:'shaber',home:'khulna'}))
+
+myarray(arrayData)
